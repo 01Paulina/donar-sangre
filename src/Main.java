f8e4f38c11491d8fe7308dc32cf73944a89195ef
01paulina <paulinalondonodiaz01>
2025-05-10 11:50:12 -0500

    se realiza uno de los ejercicios de evidencia

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner pt = new Scanner(System.in);
+        double Edad=0;
+        System.out.println("ingrese su edad:");
+        Edad= pt.nextDouble();
+
+        if (Edad >= 18 && Edad <= 65) {
+            System.out.println("Apto para donar");
+        } else{
+            System.out.println("No apto para donar");
+        }
+    }
+}
